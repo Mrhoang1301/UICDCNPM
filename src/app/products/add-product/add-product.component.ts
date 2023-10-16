4e01ba16f10c5d92a726f3622344da351278b3cf
Hoàng Super Đặng <hdang3135@gmail.com>
2023-10-16 22:18:19 +0700

    first commit

---
+++ src/app/products/add-product/add-product.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/_models/product';
+import { ProductService } from 'src/app/_services/product.service';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent implements OnDestroy{
+  model:Product
+  private addProductSubscription?: Subscription
+
+  constructor(private productService: ProductService) {
+    this.model = {
+      productName: '',
+      price: 0,
+      category:['Fashion'],
+      photoUrl:'',
+    };
+  }
+  onFormSubmit(){
+    this.addProductSubscription = this.productService.addProduct(this.model)
+    .subscribe({
+      next:(response)=>{
+        console.log(response);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.addProductSubscription?.unsubscribe();
+  }
+}
